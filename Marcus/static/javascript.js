3df4e25c9e1d88ce50a52d35699ddff7ac647cf0
John Hill <nouveau.pg@gmail.com>
2017-08-29 04:56:32 -0500

    updated ui, separated some javascript

---
+++ Marcus/static/javascript.js
@@ -0,0 +1,40 @@
+function refreshDataAjax() {
+  var data_dump = JSON.stringify({"action":"getCameraData"});
+  $.post("json-api/",data_dump,function(data,textStatus,jqXHR){
+    response_data = JSON.parse(data);
+    if (response_data['success'] == true) {
+      var camera_data = response_data["cameras"];
+      for( each in camera_data ) {
+        var timeslider_id = "#timeslider_" + each;
+        var widget = $(timeslider_id);
+        widget.slider("option","min",0);
+        widget.slider("option","max",camera_data[each]["frames"]-1);
+        widget.slider("value",camera_data[each]["frames"]-1);
+        $(timeslider_id + " span").css("left","100%");
+        d = Date(camera_data[each]['latest_frame_timestamp']);
+        console.log(d);
+        $("#timestamp_" + each).text(d.toLocaleString());
+      }
+    }
+    else {
+      alert("Error updating camera(s)!");
+    }
+  });
+}
+
+function getFrameInfo(camera_uuid,frame_offset) {
+  var data_dump = JSON.stringify({"action":"get_frame_info",
+                                  "camera_uuid":camera_uuid,
+                                "offset":frame_offset});
+  $.post("json-api/",data_dump,function(data,textStatus,jqXHR){
+    response_data = JSON.parse(data);
+    if (response_data['success'] == true) {
+      $("#camera_frame_{{ each_camera.id }}").attr('src',"uploaded_images/" + response_data['frame_url']);
+      d = Date(response_data['frame_timestamp']);
+      $("#timestamp_{{ each_camera.uuid }}").text(d.toLocaleString());
+    }
+    else {
+      alert("Error updating camera(s)!");
+    }
+  });
+}
